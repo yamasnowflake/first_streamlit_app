388dc5a075ca13c3e7d89839161c7f91cad017d9
yamasnowflake <131966064+yamasnowflake@users.noreply.github.com>
2023-04-27 21:17:54 +0900

    Update streamlit_app.py
    追加Pandas & DF

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
+import pandas
 import streamlit
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My Parents New Health Diner')
 
@@ -9,3 +12,5 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
