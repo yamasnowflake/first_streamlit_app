b04ca2355259fca5e8c1f56e6126ff3c4f17fcce
yamasnowflake <131966064+yamasnowflake@users.noreply.github.com>
2023-04-27 21:19:11 +0900

    Update streamlit_app.py
    Let's Create Some User Interaction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import pandas
 import streamlit
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 streamlit.title('My Parents New Health Diner')
 
 streamlit.header('Breakfast Menu')
@@ -13,4 +11,9 @@ streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
