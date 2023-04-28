5c1d0c4ed24cd13c7adf9f837d8c4ec5b4a38bec
yamasnowflake <131966064+yamasnowflake@users.noreply.github.com>
2023-04-28 16:27:48 +0900

    Update streamlit_app.py
    Add API calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas
 import streamlit
+import requests
 
 streamlit.title('My Parents New Health Diner')
 
@@ -21,3 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+#API Calls
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
