6a2b990b0e3674b62e9a2aa78383658d4d434406
yamasnowflake <131966064+yamasnowflake@users.noreply.github.com>
2023-04-28 16:34:41 +0900

    Update streamlit_app.py
    add fruit_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,10 @@ streamlit.dataframe(fruits_to_show)
 
 #API Calls
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #delete just output -> streamlit.text(fruityvice_response.json())
 # Read json to valiant
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
