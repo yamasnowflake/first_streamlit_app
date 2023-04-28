72139aeb5ba1bdbd0809a0d4b3d997070ed561f2
yamasnowflake <131966064+yamasnowflake@users.noreply.github.com>
2023-04-28 16:31:34 +0900

    Update streamlit_app.py
    Edit jsont to df print

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+# Read json to valiant
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write dataframe
+streamlit.dataframe(fruityvice_normalized)
