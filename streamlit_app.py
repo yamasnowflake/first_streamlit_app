6e32ac2310d3b17d12b0f367b7a842c49047169e
yamasnowflake <yama.snowflake@gmail.com>
2023-04-30 08:15:22 +0900

    フルーツ追加ボタンロジック編集

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,10 +65,20 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
+#何度もフォーム入力時に動いてしまう一時的な対応
+#streamlit.stop()
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return "Thanks for adding" + new_fruit
 
-streamlit.stop()
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
+
 #Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('what fruit add?')
-streamlit.write("Thanks for adding ", add_my_fruit)
-
-my_cur.execute("INSERT INTO fruit_load_list(FRUIT_NAME) VALUES (" + add_my_fruit + ")")
+#add_my_fruit = streamlit.text_input('what fruit add?')
+#streamlit.write("Thanks for adding ", add_my_fruit)
+#my_cur.execute("INSERT INTO fruit_load_list(FRUIT_NAME) VALUES (" + add_my_fruit + ")")
