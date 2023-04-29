95e3b344c39ca3290ba4470590c40ea095af6165
yamasnowflake <yama.snowflake@gmail.com>
2023-04-30 08:08:31 +0900

    リストロードボタン追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,13 +53,20 @@ except URLError as e:
 streamlit.stop()
 
 #lesson 12
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+
+#Add a button to load the truit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
+
 
 #Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('what fruit add?')
