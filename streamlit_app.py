1bcc1200cfe1466455c31610b4b7ce65427680e7
yamasnowflake <yama.snowflake@gmail.com>
2023-04-30 07:57:40 +0900

    ファンクション追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,15 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #API Calls
+#Create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    #delete just output -> streamlit.text(fruityvice_response.json())
+    # Read json to valiant
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+#New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -33,14 +42,13 @@ try:
         streamlit.error("Please select a fruit to get information.")
         #streamlit.write('The user entered ', fruit_choice)
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        #delete just output -> streamlit.text(fruityvice_response.json())
-        # Read json to valiant
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         # write dataframe
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_section = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_section)
 except URLError as e:
     streamlit.error()
+
+
 
 streamlit.stop()
 
